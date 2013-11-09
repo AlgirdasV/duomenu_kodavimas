ef78fa29e7dff22fd6f4afa3fac4b7b212823f5e
AlgirdasV <algirdass0@gmail.com>
2013-11-09 13:48:37 +0200

    added missing header file

---
+++ dekodavimas.h
@@ -0,0 +1,27 @@
+#ifndef DEKODAVIMAS_H
+#define DEKODAVIMAS_H
+
+#include "matrica.h"
+#include <vector>
+
+class Standart_lentele{
+	std::vector < std::vector <Elementas> > sindromai;
+	std::vector <int> vektoriu_svoriai;
+	int klasiu_kiekis;
+
+	public:
+		Standart_lentele(int );
+		void isvalyti_sindroma(int );
+		void prijungti_prie_sindromo(int, Elementas);
+		void priskirti_svori(int, int);
+		void spausdinti();
+		bool lyginti_sindromus_iki_indekso(int);
+		int rasti_svori_pagal_sindroma(Vektorius);
+};
+
+Matrica kontroline_matrica(Matrica);
+std::vector <std::vector<Elementas> > generuoti_vienetinius_vektorius(int );
+Standart_lentele skaiciuoti_sindromus(Matrica, int);
+Elementas skaliarine_sandauga(Vektorius, Vektorius);
+void dekoduoti(Vektorius , Matrica , Standart_lentele );
+#endif
