ec34f162e13a1abc2081f1f4393e2a00723ad14c
Algirdass0 <algirdass0@gmail.com>
2013-12-03 14:53:29 +0200

    added a few missing files

---
+++ source/skaidymas.h
@@ -0,0 +1,9 @@
+#ifndef SKAIDYMAS_H
+#define SKAIDYMAS_H
+#include <string>
+#include "kunas.h"
+
+	void skaidyti_faila(std::string, int);
+	Vektorius char_to_binary(char);
+	unsigned char binary_to_char(Vektorius );
+#endif
